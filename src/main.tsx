af26332405fc766adacf965ee8b212dc8df57f44
valedobrandi <valedobrandi@gmail.com>
2024-11-25 14:32:40 +0100

    icon path

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/quiz">
     <React.StrictMode>
       <App />
     </React.StrictMode>
